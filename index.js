9cb8fb0bdc72bbf7447c307ee6f607b6e916dceb
kenpeter <figo2476@hotmail.com>
2017-07-10 23:41:32 +1000

    [comment] comment

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+// use strict
 'use strict';
 
+// port 3001
 var PORT = 3001;
 
+// so we define a route like a func
 var userRoute = require('./lib/user-route');
+
+// express instance
 var app = require('express')();
 
-// Bind users route to our application
+// now just pass app into route func
 userRoute(app);
 
+// now app listent to 3001
 app.listen(PORT, function () {
   console.log('listening on port %s', PORT);
 });
--- test/user-route.js
+++ test/user-route.js
@@ -1,71 +1,104 @@
-'use strict'
+// use strict
+'use strict';
 
+// proxy require
 // We'll use this to override require calls in routes
 var proxyquire = require('proxyquire');
+
+// stub func for overwrite
 // This will create stubbed functions for our overrides
 var sinon = require('sinon');
+
+// super test, like request
 // Supertest allows us to make requests against an express object
 var supertest = require('supertest');
+
+// chai expect
 // Natural language-like assertions
 var expect = require('chai').expect;
 
+// express
 var express = require('express');
 
+// get ping
 describe('GET /ping', function () {
+  // app
+  // get user stub
+  // request
+  // route
   var app, getUserStub, request, route;
 
+  // before each
   beforeEach(function () {
-    // A stub we can use to control conditionals
+    // sinon create some dummy data
     getUserStub = sinon.stub();
 
+    // express
     // Create an express application object
     app = express();
 
+    // route......
     // Get our router module, with a stubbed out users dependency
     // we stub this out so we can control the results returned by
     // the users module to ensure we execute all paths in our code
+
+    // route, proxy require
+    // with actual user route
+    // now we have a new ../lib/user-route.js returns
     route = proxyquire('../lib/user-route.js', {
+      // inside ../lib/user-route.js
+      // we require ./users and users.getByUsername, now we override with sinon.stub();
       './users': {
         getByUsername: getUserStub
       }
     });
 
-    // Bind our application to
+    // now call ../lib/user-route.js
     route(app);
 
-    // Get a supertest instance so we can make requests
+    // also pass express instance to request.
     request = supertest(app);
   });
 
+  // it
+  // annoy func with done
   it('should respond with a 404 and a null', function (done) {
+    // what exact is this??????????????????
+    // basically ask getUserStub to returns null, user is null, see user-route.js code
     getUserStub.returns(null);
 
+    // super test with express instance
     request
-      .get('/users/nodejs')
-      .expect('Content-Type', /json/)
-      .expect(404, function (err, res) {
-        expect(res.body).to.deep.equal({
-          status: 'not ok',
-          data: null
+      .get('/users/nodejs') // access /users/:username
+      .expect('Content-Type', /json/) // expect content type, contain json
+      .expect(404, function (err, res) { // expect 404 with err and res
+        expect(res.body).to.deep.equal({ // expect, (res.body), to.deep.equal????
+          status: 'not ok', // status not ok
+          data: null // data null
         });
-        done();
+        done(); // done
       });
   });
 
+  // it
+  // func done
   it('should respond with 200 and a user object', function (done) {
+    // user data
     var userData = {
       username: 'nodejs'
     };
 
+    // now getUserStub returns {username: 'nodejs'}
     getUserStub.returns(userData);
 
+    //
     request
-      .get('/users/nodejs')
-      .expect('Content-Type', /json/)
-      .expect(200, function (err, res) {
-        expect(res.body).to.deep.equal({
-          status: 'ok',
-          data: userData
+      .get('/users/nodejs') //
+      .expect('Content-Type', /json/) //
+      .expect(200, function (err, res) { //
+        expect(res.body).to.deep.equal({ //
+          status: 'ok', //
+          data: userData //
         });
         done();
       });
